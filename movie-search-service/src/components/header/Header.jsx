d80223fa3a676e3e6099b12c1609ca4665039150
MeangSung <aoddlsgh98@gmail.com>
2024-08-21 00:40:40 +0900

    merge content

---
+++ movie-search-service/src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './HeaderCss.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1>영화 검색</h1>
+        </div>
+    );
+}
